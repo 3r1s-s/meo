ed21f349e1ff4a1b74790031fd3506231c71ce15
3r1s_s <127928354+3r1s-s@users.noreply.github.com>
2024-01-14 21:53:43 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -301,8 +301,7 @@ function loadpost(p) {
                         embeddedElement.setAttribute('data-share-method', 'host');
                         embeddedElement.setAttribute('data-width', '400px');
                         embeddedElement.setAttribute('data-height', '400px');
-                        embeddedElement.setAttribute('data-border-radius', '5px');
-
+                        embeddedElement.setAttribute('data-style', 'border-radius: 5px');
 
                         embeddedElement.classList.add("embed");
 
@@ -452,10 +451,11 @@ function dopostwizard() {
     document.getElementById('msg').value = "";
 }
 
+
 function loadhome() {
     page = "home";
     var pageContainer = document.getElementById("main");
-    pageContainer.innerHTML = `<div class='info'><h1>Home</h1><p id='info'></p></div><div class='message-container'><input type='text' class='message-input text' id='msg' autocomplete='false' placeholder='Whats on your mind?'><button class='message-send button' id='submit' value='Post!' onclick='dopostwizard()'><svg aria-hidden='true' role='img' class='sendIcon__461ff' width='16' height='16' viewBox='0 0 16 16'><path d='M8.2738 8.49222L1.99997 9.09877L0.349029 14.3788C0.250591 14.691 0.347154 15.0322 0.595581 15.246C0.843069 15.4597 1.19464 15.5047 1.48903 15.3613L15.2384 8.7032C15.5075 8.57195 15.6781 8.29914 15.6781 8.00007C15.6781 7.70101 15.5074 7.4282 15.2384 7.29694L1.49839 0.634063C1.20401 0.490625 0.852453 0.535625 0.604941 0.749376C0.356493 0.963128 0.259941 1.30344 0.358389 1.61563L2.00932 6.89563L8.27093 7.50312C8.52405 7.52843 8.71718 7.74125 8.71718 7.99531C8.71718 8.24938 8.52406 8.46218 8.27093 8.4875L8.2738 8.49222Z' fill='currentColor'></path></svg></button></div><div id='msgs' class='posts'></div>`;
+    pageContainer.innerHTML = `<div class='info'><h1>Home</h1><p id='info'></p></div><div class='message-container'><input type='text' class='message-input text' id='msg' autocomplete='false' placeholder='What&apos;s on your mind?'><button class='message-send button' id='submit' value='Post!' onclick='dopostwizard()'><svg aria-hidden='true' role='img' class='sendIcon__461ff' width='16' height='16' viewBox='0 0 16 16'><path d='M8.2738 8.49222L1.99997 9.09877L0.349029 14.3788C0.250591 14.691 0.347154 15.0322 0.595581 15.246C0.843069 15.4597 1.19464 15.5047 1.48903 15.3613L15.2384 8.7032C15.5075 8.57195 15.6781 8.29914 15.6781 8.00007C15.6781 7.70101 15.5074 7.4282 15.2384 7.29694L1.49839 0.634063C1.20401 0.490625 0.852453 0.535625 0.604941 0.749376C0.356493 0.963128 0.259941 1.30344 0.358389 1.61563L2.00932 6.89563L8.27093 7.50312C8.52405 7.52843 8.71718 7.74125 8.71718 7.99531C8.71718 8.24938 8.52406 8.46218 8.27093 8.4875L8.2738 8.49222Z' fill='currentColor'></path></svg></button></div><div id='msgs' class='posts'></div>`;
     var pageContainer = document.getElementById("nav");
     pageContainer.innerHTML = "<div class='navigation'><input type='button' class='navigation-button button' id='submit' value='Settings' onclick='loadstgs()'><input type='button' class='navigation-button button' id='submit' value='Logout' onclick='logout(false)'></div>";
     var psbox = document.getElementById("msg");
@@ -540,6 +540,10 @@ function loadstgs() {
         <button class="icon-button"><img class="icon" src="images/Mars.png" width="64px"></button>
         <button class="icon-button"><img class="icon" src="images/Orange Gradient.png" width="64px"></button>
         <button class="icon-button"><img class="icon" src="images/Orange Solid.png" width="64px"></button>
+        <button class="icon-button"><img class="icon" src="images/Meower iOS Stripe.png" width="64px"></button>
+        <button class="icon-button"><img class="icon" src="images/Meower iOS.png" width="64px"></button>
+        <button class="icon-button"><img class="icon" src="images/Revolt.png" width="64px"></button>
+        <button class="icon-button"><img class="icon" src="images/Flamingo.png" width="64px"></button>
     </div>
     <hr>
     <input type="button" class="navigation-button button" id="submit" value="Go Home" onclick="loadhome()"></div>`;
