927b3bfdc91f3e51fd2220a60c329e7e20446438
3r1s_s <127928354+3r1s-s@users.noreply.github.com>
2024-01-15 17:34:21 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -215,9 +215,9 @@ function loadpost(p) {
                 return `<a href="${url.replace(/<\/?blockquote>/g, '')}" target="_blank">${url}</a>`;
             });
 
-            var isOnlyEmojis = /^\p{Emoji}+$/u.test(content);
+            var isemoji = /[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{1F700}-\u{1F77F}\u{1F780}-\u{1F7FF}\u{1F800}-\u{1F8FF}\u{1F900}-\u{1F9FF}\u{1FA00}-\u{1FA6F}\u{2600}-\u{26FF}\u{2700}-\u{27BF}]+/u.test(content);
 
-            if (isOnlyEmojis) {
+            if (isemoji) {
                 postContentText.classList.add("big");
             }
         
