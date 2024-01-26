51aa3e167dc51df71dca11e1118c91d984ff908b
3r1s_s <127928354+3r1s-s@users.noreply.github.com>
2024-01-26 00:24:20 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -108,10 +108,16 @@ function main() {
         
     };
     document.addEventListener("keydown", function(event) {
-        if (event.key === "Enter") {
+        var messageInput = document.getElementById("msg");
+    
+        if (event.key === "Enter" && !event.shiftKey) {
+            event.preventDefault();
             dopostwizard();
+        } else if (event.key === "Enter" && event.shiftKey) {
+            messageInput.value += "\n";
         }
     });
+    
 
 }
 
@@ -159,41 +165,13 @@ function loadpost(p) {
     postContainer.appendChild(postContent);
 
     var postContentText = document.createElement("p");
+    
     blist = ["", " ", "# ", "## ", "### ", "#### ", "##### ", "###### ", "\n"];
     if (!blist.includes(content)) {
-        var splc = content.split("\n");
-        var asplc = "";
-        for (const i in splc) {
-            if (i == splc.length - 1) {
-                asplc += splc[i];
-            } else {
-                asplc += splc[i] + "\n\n";
-            }
-        }
-       
-        const timestampRegex = /<t:(\d+):(t|R|T|d|D|f|F)>/g;
-
-        var escapedInput = asplc.replace(timestampRegex, (match, timestamp, format) => {
-            const date = new Date(parseInt(timestamp) * 1000);
-            switch (format) {
-                case 't':
-                    return date.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
-                case 'R':
-                    return formatTime(date.getTime());
-                case 'T':
-                    return date.toLocaleTimeString('en-US');
-                case 'd':
-                    return date.toLocaleDateString('en-US');
-                case 'D':
-                    return date.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
-                case 'f':
-                    return date.toLocaleString('en-US', { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true });
-                case 'F':
-                    return date.toLocaleString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true });
-                default:
-                    return match;
-            }
-        })
+        var asplc = content;
+        
+        
+        var escapedInput = asplc
         .replace(/&/g, '&amp;')
         .replace(/</g, '&lt;')
         .replace(/>/g, '&gt;')
@@ -213,16 +191,22 @@ function loadpost(p) {
             .replace(/~~([\s\S]*?)~~/g, '<del>$1</del>')
             .replace(/(?:https?|ftp):\/\/[^\s(){}[\]]+/g, function(url) {
                 return `<a href="${url.replace(/<\/?blockquote>/g, '')}" target="_blank">${url}</a>`;
-            });
+            })
+           
+            var textContent = escapedInput
+            .replace(/&lt;:(\w+):(\d+)&gt;/g, '<img src="https://cdn.discordapp.com/emojis/$2.webp?size=96&quality=lossless" alt="$1" width="16px" class="emoji">')
+            .replace(/&lt;a:(\w+):(\d+)&gt;/g, '<img src="https://cdn.discordapp.com/emojis/$2.gif?size=96&quality=lossless" alt="$1" width="16px" class="emoji">') 
+            
+            .replace(/\n/g, '<br>');
+            
 
+            
             var isEmoji = /^[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{1F700}-\u{1F77F}\u{1F780}-\u{1F7FF}\u{1F800}-\u{1F8FF}\u{1F900}-\u{1F9FF}\u{1FA00}-\u{1FA6F}\u{2600}-\u{26FF}\u{2700}-\u{27BF}]+$/u.test(content);
 
             if (isEmoji) {
                 postContentText.classList.add("big");
             }
             
-        
-
         postContentText.innerHTML = textContent;
 
         postContentText.querySelectorAll('p a').forEach(link => {
@@ -468,7 +452,7 @@ function dopostwizard() {
 function loadhome() {
     page = "home";
     var pageContainer = document.getElementById("main");
-    pageContainer.innerHTML = `<div class='info'><h1>Home</h1><p id='info'></p></div><div class='message-container'><input type='text' class='message-input text' id='msg' autocomplete='false' placeholder='What&apos;s on your mind?'><button class='message-send button' id='submit' value='Post!' onclick='dopostwizard()'><svg aria-hidden='true' role='img' class='sendIcon__461ff' width='16' height='16' viewBox='0 0 16 16'><path d='M8.2738 8.49222L1.99997 9.09877L0.349029 14.3788C0.250591 14.691 0.347154 15.0322 0.595581 15.246C0.843069 15.4597 1.19464 15.5047 1.48903 15.3613L15.2384 8.7032C15.5075 8.57195 15.6781 8.29914 15.6781 8.00007C15.6781 7.70101 15.5074 7.4282 15.2384 7.29694L1.49839 0.634063C1.20401 0.490625 0.852453 0.535625 0.604941 0.749376C0.356493 0.963128 0.259941 1.30344 0.358389 1.61563L2.00932 6.89563L8.27093 7.50312C8.52405 7.52843 8.71718 7.74125 8.71718 7.99531C8.71718 8.24938 8.52406 8.46218 8.27093 8.4875L8.2738 8.49222Z' fill='currentColor'></path></svg></button></div><div id='msgs' class='posts'></div>`;
+    pageContainer.innerHTML = `<div class='info'><h1>Home</h1><p id='info'></p></div><div class='message-container'><textarea type='text' class='message-input text' id='msg' rows='1' autocomplete='false' placeholder='What&apos;s on your mind?'></textarea><button class='message-send button' id='submit' value='Post!' onclick='dopostwizard()'><svg aria-hidden='true' role='img' class='sendIcon__461ff' width='16' height='16' viewBox='0 0 16 16'><path d='M8.2738 8.49222L1.99997 9.09877L0.349029 14.3788C0.250591 14.691 0.347154 15.0322 0.595581 15.246C0.843069 15.4597 1.19464 15.5047 1.48903 15.3613L15.2384 8.7032C15.5075 8.57195 15.6781 8.29914 15.6781 8.00007C15.6781 7.70101 15.5074 7.4282 15.2384 7.29694L1.49839 0.634063C1.20401 0.490625 0.852453 0.535625 0.604941 0.749376C0.356493 0.963128 0.259941 1.30344 0.358389 1.61563L2.00932 6.89563L8.27093 7.50312C8.52405 7.52843 8.71718 7.74125 8.71718 7.99531C8.71718 8.24938 8.52406 8.46218 8.27093 8.4875L8.2738 8.49222Z' fill='currentColor'></path></svg></button></div><div id='msgs' class='posts'></div>`;
     var pageContainer = document.getElementById("nav");
     pageContainer.innerHTML = "<div class='navigation'><input type='button' class='navigation-button button' id='submit' value='Settings' onclick='loadstgs()'><input type='button' class='navigation-button button' id='submit' value='Logout' onclick='logout(false)'></div>";
     var psbox = document.getElementById("msg");
@@ -654,8 +638,6 @@ function formatTime(timestamp) {
 
     return days + (days === 1 ? ' day ago' : ' days ago');
 }
-
-
 
 function ping() {
     webSocket.send(JSON.stringify({
