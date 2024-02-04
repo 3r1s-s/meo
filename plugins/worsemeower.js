12f72d3e518c314d7ddb9dbe3f95d2d177c05ec5
3r1s_s <127928354+3r1s-s@users.noreply.github.com>
2024-02-03 22:30:34 -0700

    add bloctans thing

--- plugins/worsemeower.js
+++ plugins/worsemeower.js
@@ -28,9 +28,9 @@
       .replace(")", "(")
       .replace("L", "I")
       .replace("A", "∀")
-      .replace("B", "👌︎") +
+      .replace("B", "👌") +
     "?????? (This post was made with https://meo-32r.pages.dev/)";
-})();
+}
 // bloctans
 var html = document.querySelector("html")
 html.classList = []
@@ -38,3 +38,4 @@ html.style = "--background-color: hotpink; --color: #000000; --accent-color: #FF
 var stylesnew = document.createElement("style")
 stylesnew.innerText = ".button, .cstpgbt, input { border: solid 10px var(--accent-color); border-radius: 100% !important; }"
 document.head.appendChild(stylesnew)
+)();
