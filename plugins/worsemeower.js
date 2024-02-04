3389e6e056e4f0a9b74a7249df8ef56c3db1d53f
3r1s_s <127928354+3r1s-s@users.noreply.github.com>
2024-02-03 22:28:03 -0700

    Update worsemeower.js

--- plugins/worsemeower.js
+++ plugins/worsemeower.js
@@ -1,3 +1,4 @@
+// mybearworld
 (() => {
   const originalDoPostWizard = window.dopostwizard;
   window.dopostwizard = () => {
@@ -30,3 +31,10 @@
       .replace("B", "👌︎") +
     "?????? (This post was made with https://meo-32r.pages.dev/)";
 })();
+// bloctans
+var html = document.querySelector("html")
+html.classList = []
+html.style = "--background-color: hotpink; --color: #000000; --accent-color: #FFFFFF; --hov-accent-color: #FF0000; --hov-color: var(--hov-accent-color);"
+var stylesnew = document.createElement("style")
+stylesnew.innerText = ".button, .cstpgbt, input { border: solid 10px var(--accent-color); border-radius: 100% !important; }"
+document.head.appendChild(stylesnew)
