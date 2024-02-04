314224826a15921432cd8375a9343ece09004e6d
3r1s_s <127928354+3r1s-s@users.noreply.github.com>
2024-02-03 22:40:00 -0700

    fix worsemeower.js

--- plugins/worsemeower.js
+++ plugins/worsemeower.js
@@ -7,6 +7,14 @@
 
     originalDoPostWizard();
   };
+  
+  // bloctans
+  var html = document.querySelector("html")
+  html.classList = []
+  html.style = "--background-color: hotpink; --color: #000000; --accent-color: #FFFFFF; --hov-accent-color: #FF0000; --hov-color: var(--hov-accent-color);"
+  var stylesnew = document.createElement("style")
+  stylesnew.innerText = ".button, .cstpgbt, input { border: solid 10px var(--accent-color); border-radius: 100% !important; }"
+  document.head.appendChild(stylesnew)
 
   const beautify = (string) =>
     string
@@ -31,11 +39,4 @@
       .replace("B", "👌") +
     "?????? (This post was made with https://meo-32r.pages.dev/)";
 }
-// bloctans
-var html = document.querySelector("html")
-html.classList = []
-html.style = "--background-color: hotpink; --color: #000000; --accent-color: #FFFFFF; --hov-accent-color: #FF0000; --hov-color: var(--hov-accent-color);"
-var stylesnew = document.createElement("style")
-stylesnew.innerText = ".button, .cstpgbt, input { border: solid 10px var(--accent-color); border-radius: 100% !important; }"
-document.head.appendChild(stylesnew)
 )();
