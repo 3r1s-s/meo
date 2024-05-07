9d93f2e565aaa852254ae2791eb8972cefa3909f
3r1s_s <127928354+3r1s-s@users.noreply.github.com>
2024-05-06 17:17:19 -0700

    Update meoads.js

--- plugins/meoads.js
+++ plugins/meoads.js
@@ -4,8 +4,6 @@ javascript:(function() {
 <center style="position: absolute;bottom:0;left:0;right:0;">
     <iframe id="BetterAd" src="https://adservice.bettermeower.app/generate" style="border: medium; width: 728px; height: 90px; overflow: hidden;" scrolling="no"></iframe>
     <iframe id="BetterAd" src="https://adservice.bettermeower.app/generate" style="border: medium; width: 728px; height: 90px; overflow: hidden;" scrolling="no"></iframe>
-    <iframe id="BetterAd" src="https://adservice.bettermeower.app/generate" style="border: medium; width: 728px; height: 90px; overflow: hidden;" scrolling="no"></iframe>
-    <iframe id="BetterAd" src="https://adservice.bettermeower.app/generate" style="border: medium; width: 728px; height: 90px; overflow: hidden;" scrolling="no"></iframe>
 </center>
     `
   document.body.appendChild(a);
