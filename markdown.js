a2593ad985c3cb8c73ae975b6f065e8f6e441833
3r1s_s <127928354+3r1s-s@users.noreply.github.com>
2024-06-11 22:20:56 -0700

    fixed some @username things

--- markdown.js
+++ markdown.js
@@ -37,8 +37,9 @@ function escapeHTML(content) {
 }
 
 function erimd(content) {
+    console.log(content)
     const text = content
-        .replace(/@([\w-]+)/g, '<span id="username" class="attachment" onclick="openUsrModal(\'$1\')">@$1</span>')
+        .replace(/@([\w-]+)(?![^<]*?<\/code>)/g, '<span id="username" class="attachment" onclick="openUsrModal(\'$1\')">@$1</span>')
         .replace(/&lt;:(\w+):(\d+)&gt;/g, '<img src="https://cdn.discordapp.com/emojis/$2.webp?size=96&quality=lossless" alt="$1" title="$1" class="emoji">')
         .replace(/&lt;a:(\w+):(\d+)&gt;/g, '<img src="https://cdn.discordapp.com/emojis/$2.gif?size=96&quality=lossless" alt="$1" title="$1" class="emoji">');
     return text;
