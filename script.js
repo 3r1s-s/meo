4e308da90dce2b6d478d1dfcb75d398cc4501f16
3r1s_s <127928354+3r1s-s@users.noreply.github.com>
2024-06-28 14:07:49 -0700

    theme bug fix

--- script.js
+++ script.js
@@ -877,6 +877,7 @@ function loadtheme() {
     const theme = localStorage.getItem("theme");
     
     if (theme) {
+        document.documentElement.classList.remove("dark-theme");
         document.documentElement.classList.add(theme + "-theme");
     }
     
