bd30fbf8d5fec1042828364c1ce8189b28a33c7e
3r1s_s <127928354+3r1s-s@users.noreply.github.com>
2024-07-29 20:07:38 -0700

    Some things

--- script.js
+++ script.js
@@ -450,7 +450,6 @@ function loadLogin() {
             <option value="es" ${language === "es" ? "selected" : ""}>${es.language}</option>
             <option value="de" ${language === "de" ? "selected" : ""}>${de.language}</option>
             <option value="ua" ${language === "ua" ? "selected" : ""}>${ua.language}</option>
-            <option value="sj" ${language === "sj" ? "selected" : ""}>${sj.language}</option>
         </select>
         </div>
         <div class="login-back">
@@ -2641,12 +2640,11 @@ function loadLanguages() {
             <button class="language button" id="es_es" onclick="changeLanguage('es_es')"><span class="language-l">${es_es.language}</span><span class="language-r">Spanish (Spain)</span><div class="radio"></div></button>
             <button class="language button" id="de" onclick="changeLanguage('de')"><span class="language-l">${de.language}</span><span class="language-r">German</span><div class="radio"></div></button>
             <button class="language button" id="ua" onclick="changeLanguage('ua')"><span class="language-l">${ua.language}</span><span class="language-r">Ukrainian</span><div class="radio"></div></button>
-            <button class="language button" id="sj" onclick="changeLanguage('sj')"><span class="language-l">${sj.language}</span><span class="language-r">Sujaliro</span><div class="radio"></div></button>
             <h3>${lang().languages_sub.other}</h3>
+            <button class="language button" id="sj" onclick="changeLanguage('sj')"><span class="language-l">${sj.language}</span><span class="language-r">Sujaliro</span><div class="radio"></div></button>
             <button class="language button" id="eh" onclick="changeLanguage('eh')"><span class="language-l">${eh.language}</span><span class="language-r">Enchantment Table</span><div class="radio"></div></button>
             <button class="language button" id="b" onclick="changeLanguage('b')"><span class="language-l">${b.language}</span><span class="language-r">Bottom</span><div class="radio"></div></button>
             <button class="language button" id="owo" onclick="changeLanguage('owo')"><span class="language-l">${owo.language}</span><span class="language-r">owo</span><div class="radio"></div></button>
-            <button class="language button" id="cowboy" onclick="changeLanguage('cowboy')"><span class="language-l">${cowboy.language}</span><span class="language-r">Cowboy</span><div class="radio"></div></button>
             <button class="language button" id="eris" onclick="changeLanguage('eris')"><span class="language-l">${eris.language}</span><span class="language-r">Eris</span><div class="radio"></div></button>
             <button class="language button" id="goobert" onclick="changeLanguage('goobert')"><span class="language-l">${goobert.language}</span><span class="language-r">goobert</span><div class="radio"></div></button>
         </div>
